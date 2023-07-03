cd9db6418be3262fecd6931e662862f36a1dff44
Benjamin <benjamin.shum13@hotmail.com>
2023-07-03 17:40:00 +0800

    End of Section 5

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,19 +6,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DateInputsModule } from '@progress/kendo-angular-dateinputs';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { ButtonsModule, DropDownButtonModule } from '@progress/kendo-angular-buttons';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     DateInputsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    ButtonsModule,
+    DropDownButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
