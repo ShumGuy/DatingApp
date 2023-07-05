7223649334ca858a9bdc156928ddf68a6b16bad1
Benjamin <benjamin.shum13@hotmail.com>
2023-07-05 14:19:34 +0800

    End of Section 7

--- API/DTOs/RegisterDto.cs
+++ API/DTOs/RegisterDto.cs
@@ -1,13 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace API.DTOs
 {
     public class RegisterDto
     {
         public string Username{ get; set; }
+
+        [Required]
+        [StringLength(8, MinimumLength = 4)]
         public string Password{ get; set; }
     }
 }
